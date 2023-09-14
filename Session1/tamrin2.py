703823e5530e3ad00f988578b2835466ecb999e0
PC <httdiyversion2.0.1@gmail.com>
2023-09-14 09:14:55 +0330

    PC

--- Session1/tamrin2.py
+++ Session1/tamrin2.py
@@ -3,5 +3,22 @@ inp2 = int(input())
 
 bmm = 0
 kmm = 0
+numbmm = 1
+numkmm = 1
+k = 0
+if inp1 > inp2:
+    b = inp1
+else:
+    b = inp2
 while True:
-    bmm
+    if numbmm > b/2:
+        break
+    else:
+        if int(inp1/numbmm) == inp1/numbmm and int(inp2/numbmm) == inp2/numbmm:
+            bmm += numbmm
+    numbmm += 1
+while True:
+    var = inp2*inp1
+    if int(var/numkmm) == var/numkmm:
+        kmm += numkmm
+    numkmm += 1
