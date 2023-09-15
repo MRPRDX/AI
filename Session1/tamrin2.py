ad31f4839e4f763cc327d1be76b53620559a00a0
PC <httdiyversion2.0.1@gmail.com>
2023-09-15 10:20:02 +0330

    PC

--- Session1/tamrin2.py
+++ Session1/tamrin2.py
@@ -1,24 +1,20 @@
 inp = input()
-index = 0
-for t in inp:
-    if inp[index] == " ":
-        inp1 = int(inp[:index])
-        inp2 = int(inp[index:])
-    index += 1
+index = inp.index(" ")
+n = int(inp[:index])
+m = int(inp[index:])
+# for t in inp:
+#     if inp[index] == " ":
+#         n = int(inp[:index])
+#         m = int(inp[index:])
+#     index += 1
 bmm = []
 b = 1
-if inp1>inp2:
-    big = int(inp1)
-    small = int(inp2)
-else:
-    big = inp2
-    small = inp1
-for r in range(int(big/2)):
-    if int(inp1/(r+1)) == inp1/(r+1) and int(inp2/(r+1)) == inp2/(r+1):
+for r in range(int(n)):
+    if int(n/(r+1)) == n/(r+1) and int(m/(r+1)) == m/(r+1):
         bmm.append(r+1)
 
-if int(big/small) == big/small:
-    kmm = big
+if (n/max(bmm)) * (m/max(bmm)) < m or (n/max(bmm)) * (m/max(bmm)) == m:
+    kmm = (n/max(bmm)) * (m/max(bmm)) * max(bmm)
 else:
-    kmm = max(bmm) * big 
-print(str(max(bmm))+" "+str(kmm))
+    kmm = (n/max(bmm)) * (m/max(bmm))
+print(str(max(bmm))+" "+str(int(kmm)))
--- Session1/tamrin5.py
+++ Session1/tamrin5.py
@@ -2,9 +2,7 @@ a = int(input())
 b = int(input())
 t = b
 if a%2 == 0:
-    satr = 1
-elif a == 3:
-    satr = -1
+    satr = 0
 else:
     satr = 0
 if a > b:
@@ -12,7 +10,7 @@ if a > b:
         if (a-b)%2 == 1:
             print("Wrong difference!")
             break
-        if satr == int((a/2)-1):
+        if satr == ((a-b)/2):
             while t > 0:
                 satr += 1
                 print((int((a-b)/2))*"* "+b*"  "+(int(((a-b)/2)-1))*"* "+"*")
@@ -26,11 +24,11 @@ if a > b:
                     else:
                         print((a-1)*"* "+"*", end="\n")
             else:
-                if satr <= a:
+                if satr < a:
                     satr += 1
                     if satr == a:
                         print((a-1)*"* "+"*")
                     else:
                         print((a-1)*"* "+"*", end="\n")
-elif a == b or b > a:
+elif a == b or b > a or b >= 20:
     print("Wrong order!")
