38c1835e3bb27280a1395eedef4718cbbf5041ac
PC <httdiyversion2.0.1@gmail.com>
2023-09-15 11:35:57 +0330

    PC

--- Session1/tamrin2.py
+++ Session1/tamrin2.py
@@ -2,20 +2,20 @@ inp = input()
 index = inp.index(" ")
 n = int(inp[:index])
 m = int(inp[index:])
-# for t in inp:
-#     if inp[index] == " ":
-#         n = int(inp[:index])
-#         m = int(inp[index:])
-#     index += 1
 bmm = []
 b = 1
-for r in range(int(n)):
+
+r = 0
+while True:
     if int(n/(r+1)) == n/(r+1) and int(m/(r+1)) == m/(r+1):
         bmm.append(r+1)
-
-if (n/max(bmm)) * (m/max(bmm)) < m or (n/max(bmm)) * (m/max(bmm)) == m:
-    kmm = (n/max(bmm)) * (m/max(bmm)) * max(bmm)
-    # for example for 34 and 36 it's wrong
-else:
-    kmm = (n/max(bmm)) * (m/max(bmm))
+    if r == int(n/2):
+        break
+    r += 1
+i = n/max(bmm)
+while True:
+    if int((m*i)/n) == (m*i)/n:
+        kmm = m*i
+        break
+    i -= 1
 print(str(max(bmm))+" "+str(int(kmm)))
