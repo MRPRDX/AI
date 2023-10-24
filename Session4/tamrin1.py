ee00ab5988bac4ad9247b159b1f952ccceb1a867
Sony <httdiyversion2.0.1@gmail.com>
2023-10-24 03:47:08 -0700

    Laptop

---
+++ Session4/tamrin1.py
@@ -0,0 +1,9 @@
+def sorter():
+    inp = input().split()
+    lis = list(inp)
+    char2list = []
+    for i in lis:
+        char2list.append(i[0:2].upper())
+        char2list.sort()
+    zip
+sorter()
