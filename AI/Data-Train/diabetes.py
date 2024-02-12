890f1c5b0e34795255c466e0a92b56dfa994007b
PC <httdiyversion2.0.1@gmail.com>
2024-02-12 22:28:24 +0330

    PC

--- AI/Data-Train/diabetes.py
+++ AI/Data-Train/diabetes.py
@@ -4,12 +4,13 @@ from sklearn import svm
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import accuracy_score, recall_score, precision_score
-df = pd.read_csv('./op_webcam.csv')
+from sklearn.linear_model import LogisticRegression
+df = pd.read_csv("./diabetes.csv")
 df.head()
 
-df['EAR'].value_counts()
-X = df.drop('EAR', axis=1)
-y = df['EAR']
+df['Outcome'].value_counts()
+X = df.drop('Outcome', axis=1)
+y = df['Outcome']
 
 X = np.array(X)
 y = np.array(y)
@@ -19,7 +20,7 @@ X = scaler.fit_transform(X)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-model = svm.SVC(kernel='')
+model = svm.SVC(kernel="rbf")
 model.fit(X_train, y_train)
 
 y_pred_train = model.predict(X_train)
@@ -32,3 +33,8 @@ print(acc_test, acc_train)
 p = precision_score(y_test, y_pred_test)
 r = recall_score(y_test, y_pred_test)
 print(p,r)
+
+# Logestic regression
+
+
+
